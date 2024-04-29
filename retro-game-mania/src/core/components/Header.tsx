6539ce14cfc884b50a675e29e728e2a9fddffa83
pseudogode <jordakiev.online@gmail.com>
2024-04-29 17:16:13 +0000

    Install drag and drop library

--- retro-game-mania/src/core/components/Header.tsx
+++ retro-game-mania/src/core/components/Header.tsx
@@ -2,9 +2,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { Paper } from '@mui/material';
 
 export default function ButtonAppBar() {
@@ -12,17 +9,9 @@ export default function ButtonAppBar() {
     <Box>
       <AppBar sx={{position: "static"}}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="primary"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component={Paper} >
             home
           </Typography>
-          <Button sx={{color: "inherit"}}>Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
